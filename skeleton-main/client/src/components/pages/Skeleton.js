f1eb1cedf79f535c57d488681c884268163c836a
shadowdragon2805 <lexue28@gmail.com>
2023-02-19 10:01:01 -0500

    connected to db

--- skeleton-main/client/src/components/pages/Skeleton.js
+++ skeleton-main/client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "FILL ME IN";
+const GOOGLE_CLIENT_ID = "518418044074-3o2nk4767heckhbvpo6pe1bfkvf3s361.apps.googleusercontent.com";
 
 const Skeleton = ({ userId, handleLogin, handleLogout }) => {
   return (
--- skeleton-main/server/server.js
+++ skeleton-main/server/server.js
@@ -36,9 +36,9 @@ const socketManager = require("./server-socket");
 
 // Server configuration below
 // TODO change connection URL after setting up your team database
-const mongoConnectionURL = process.env.MONGO_SRV;
+const mongoConnectionURL = "mongodb+srv://jessemagni:UafGADi5hMvi5M4i@blueprint2023.s0i3gdl.mongodb.net/?retryWrites=true&w=majority";
 // TODO change database name to the name you chose
-const databaseName = "FILL_ME_IN";
+const databaseName = "Blueprint2023";
 
 // mongoose 7 warning
 mongoose.set("strictQuery", false);
