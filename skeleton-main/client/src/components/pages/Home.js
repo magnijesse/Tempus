8b46031f3378d99f991b05dca0ebb6a909638d39
Arjun Mattewada <arjunmattewada@gmail.com>
2023-02-19 13:27:36 -0600

    updated home page 2

--- skeleton-main/client/src/components/pages/Home.js
+++ skeleton-main/client/src/components/pages/Home.js
@@ -5,13 +5,25 @@ import logo from '../../../dist/Tempus.png'
 
 import "../../utilities.css";
 import "./Home.css";
+import { ButtonHTMLAttributes } from "react";
 
 const Home = () => {
 
     return (
-        <div class="background-container">
-            <img src={logo} alt="Logo"/>
+    <div>
+        <div className="image-container">
+        <img src={logo} alt="logo"/>
         </div>
+         
+         
+            <button className="button-container">Log In</button>
+            <button className="button-container">Add New Class </button>
+            <button className="button-container">About Tempus </button>
+        
+    </div>
+           
+        
+        
     )
 
 }
