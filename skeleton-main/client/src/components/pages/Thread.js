33e4256a40659926c4a6de9cd949f20a90714e9f
shadowdragon2805 <lexue28@gmail.com>
2023-02-19 16:11:29 -0500

    help

--- skeleton-main/client/src/components/pages/Thread.js
+++ skeleton-main/client/src/components/pages/Thread.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
 import Message from "../modules/Message";
 import NewMessage from "../modules/NewMessage";
 import "../../utilities.css";
+import { get, post } from "../../utilities";
+
+
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
 const GOOGLE_CLIENT_ID = "518418044074-3o2nk4767heckhbvpo6pe1bfkvf3s361.apps.googleusercontent.com";
