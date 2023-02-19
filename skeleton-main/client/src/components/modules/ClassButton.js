bcf01925efc53223e4b42632a922ca03d06cf525
Jesse Magni <jessemagni@Jesses-MacBook-Air.local>
2023-02-19 11:11:04 -0500

    started courseButton component

---
+++ skeleton-main/client/src/components/modules/ClassButton.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ClassButton = (props) => {
+    
+
+    
+    return (
+        <button>
+            {props.classID}
+        </button>
+    )
+
+}
+
+export default ClassButton
