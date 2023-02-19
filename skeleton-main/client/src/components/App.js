25c57e0f31a8d1649481f8c1ad120cc85bffc63a
Arjun Mattewada <arjunmattewada@gmail.com>
2023-02-19 10:54:29 -0600

    updated routes

--- skeleton-main/client/src/components/App.js
+++ skeleton-main/client/src/components/App.js
@@ -58,7 +58,7 @@ const App = () => {
       />
       <Route path="*" element={<NotFound />} />
       <Route
-        path="/home"
+        path="/"
         element={
           <Home
             path="/"
