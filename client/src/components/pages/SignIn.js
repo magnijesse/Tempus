1422957854356e8becd83f5457ef96c489c46009
Jesse Magni <magnijesse@gmail.com>
2023-03-04 17:43:24 -0500

    more bug fixes and organization

--- client/src/components/pages/SignIn.js
+++ client/src/components/pages/SignIn.js
@@ -6,7 +6,7 @@ import { auth } from "../../../../utils/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-import "../../styles/signin.css"
+import "../../styles/signin.css";
 
 const SignIn = () => {
   const [user, loading] = useAuthState(auth);
@@ -35,14 +35,13 @@ const SignIn = () => {
   return (
     <>
       {!user && (
-      <div className="sign-in-box">
+        <div className="sign-in-box">
           <h3>Sign in with one of the providers</h3>
           <div className="sign-in-button-div">
-          <FcGoogle />
             <button className="sign-in-button" onClick={GoogleLogin}>
               Sign in with google
             </button>
-            
+
             <button className="sign-in-button">Sign in with facebook</button>
           </div>
         </div>
